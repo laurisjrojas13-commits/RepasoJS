6f7e0a7d1a276d26ec35c36d528a29d5a7839e08
Julieth <lauris.jrojas13@gmail.com>
2025-09-02 20:12:20 -0500

    proyecto js

---
+++ 01-HolaMundo/holamundo.js
@@ -0,0 +1,31 @@
+console.log('Hola mundo');
+
+let cajaDeAndy = "Woody";
+
+console.log(cajaDeAndy);
+
+let edad = 17;
+let nombre = "julieth"
+
+
+console.log('Mi nombre es '+nombre+" y tengo "+edad+" años ");
+
+//array
+
+let frutas = ['manzanas','banano','mango'];
+
+console.log(frutas);
+
+console.log(frutas[1]);
+
+let frase ="JavaScript es extremadamente genial"
+
+console.log(frase);
+console.log(frase.length);
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+//operaciones matematicas
+const suma = 3 +12;
+
+console.log(suma);
